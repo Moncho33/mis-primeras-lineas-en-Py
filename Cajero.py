cf4582908c0448eac503303232d4b7b6e5a5f976
Moncho33 <115112152+Moncho33@users.noreply.github.com>
2024-03-17 01:42:16 -0500

    en proceso

---
+++ Cajero.py
@@ -0,0 +1,33 @@
+
+  #  while True:
+        
+'''
+    
+       '''
+
+if __name__ == '_main_':
+   registrar()
+   
+
+   '''
+   
+
+global  saldoA
+global consignar
+global saldoN
+
+def retirar():
+
+    retiro = input('ingrese la cantidad a retirar ->')
+    Nsaldo = saldoA - retiro
+
+
+def consultarSaldo():
+    print('consultar')
+
+def consinar():
+    saldoN=saldoA+consignar
+
+def nuevoSaldo():
+    print(f'nuevo saldo')
+   '''
